99f469f073601470c0866c3b4693a0404fb24248
Raul Veras <verasraul@gmail.com>
2022-04-04 17:57:09 -0400

    adding generationCurse funx

---
+++ generationCurse.js
@@ -0,0 +1,36 @@
+// Generation	M	                  F
+// -3	        great grandfather	 great grandmother
+// -2	        grandfather	       grandmother
+// -1	        father	           mother
+//  0	        me!	               me!
+//  1	        son	               daughter
+//  2	        grandson	         granddaughter
+//  3	        great grandson	   great granddaughter
+
+// create an object to represent the data
+const generationInfo = [{
+    "Generation" : "-3",
+    "M" : "great grandfather",
+    "F" : "great grandmother",
+    },
+    {
+        "Generation" : "-2",
+        "M" : "grandfater",
+        "F" : "grandmother",
+    },
+    {
+        "Generation" : "-1",
+        "M" : "father",
+        "F" : "mother",
+    }
+]
+
+function generationCurse(x,y){
+    //x is a number representing the generation
+    //y represents the sex
+    //y is equal to m or f
+  
+    //function should return who will recieve the generational curse based on the table 
+    
+  }
+
