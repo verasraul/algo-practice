84621dfc195da57910cdeb2a5ac1d91afc0ad42d
Raul Veras <verasraul@gmail.com>
2022-04-04 17:56:33 -0400

    adding generationCurse funx

--- testStringWithRegex.js
+++ testStringWithRegex.js
@@ -2,9 +2,21 @@
 
 function testStringWithRegex(str){
 
+    // let expression
+
     let expression = str.match(/mark/);
 
         return expression
 }
 
-console.log(testStringWithRegex(`my name is mark`));
+console.log(testStringWithRegex(`my name is julio`));
+
+
+
+// function regexTest(str){
+//     const regex = /^mark$/;
+    
+//     console.log(regex.test(str));
+
+// }
+// regexTest('diamon');
