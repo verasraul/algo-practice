10d53b1ac37f80c8fc87b7ba10bddc9695c51747
Raul Veras <verasraul@gmail.com>
2022-04-05 17:56:10 -0400

    created linked lists

---
+++ linkedLists.js
@@ -0,0 +1,16 @@
+
+const number = 5;
+// const log = console.log.bind(console, "Results: ")
+
+class LinkList {
+  constructor(head = null) {
+    this.head = head
+  }
+}
+
+class Node {
+  constructor( data) {
+    this.next = null
+    this.data = data
+  }
+}
