85e99cabacf60ac0c8a26acdc66f6103a7a33ca2
Raul Veras <verasraul@gmail.com>
2022-04-06 18:03:57 -0400

    added warm-up module

---
+++ algo-warmup-1.js
@@ -0,0 +1,18 @@
+//create a javascript object with three properties [JAVASCRIPT OBJECTS]
+const jsObject = {
+    "FIRST" : "Raul",
+    "MIDDLE" : "n/a"
+    "LAST" : "Veras",
+}
+
+
+//create an array with 5 elements
+//push a value into the array
+//pop a value from the array [ARRAY MANIPULATION]
+
+//create a function that uses a ternary [TERNARY]
+
+//create a function that loops through your array and prints each value [LOOPING]
+
+
+//create a function that concatenates a string [CONCATENATION]
--- linkedLists.js
+++ linkedLists.js
@@ -10,14 +10,14 @@ class LinkList {
   }
 }
 
-let first = new LinkList();
-
 class Node {
   constructor( data) {
     this.next = null
     this.data = data
   }
 }
+
+let first = new LinkList(Node);
 
 //  create function to loop through linked list
 function printList(){
@@ -28,6 +28,71 @@ function printList(){
         str += current.element + ' ';
         current = current.next;
     }
-    console.log(str)
+    console.log(str);
 }
 printList(first);
+
+
+
+
+
+
+
+// log('test')
+
+// const number = 5;
+// const log = console.log.bind(console, "Results: ")
+ // log('wren will show case the results')
+// log(number)
+
+// class LinkList {
+//   constructor(head = null) {
+//     this.head = head
+//   }
+// }
+
+// class Node {
+//   constructor( data) {
+//     this.next = null
+//     this.data = data
+//   }
+// }
+
+// const firstNode = new Node('This is data Test')
+// const secoundNode = new Node('This is secound data Test')
+// const thirdNode = new Node('This is third data Test')
+// log(firstNode)
+// log(secoundNode)
+// log(thridNode)
+
+// firstNode.next = secoundNode
+// secoundNode.next = thirdNode
+
+
+// const linkArrayList = new LinkList(firstNode )
+// log(linkArrayList.head.next)
+
+// while next is not null 
+// console new data 
+
+
+
+// IF linklist head the 
+  //showcase curruent node and  the next node 
+
+// function travistList(linkListParam) {
+
+//   if(linkListParam.head === null){
+//     return;
+// ur return no value  so that there a placeholder as head 
+//   } else { 
+    // get the starting node variable 
+//   const currentNode = linkListParam.head
+//   while(currentNode.next != null){
+//     log(currentNode)
+//     currentNode = currentNode.next
+//     log(currentNode.data)
+//         } 
+//     }
+// }
+// travistList(linkArrayList)
